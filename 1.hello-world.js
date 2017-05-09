155c8dbe6ddd45f099908b03a71fe3538d15fda6
Taulut Hossain Washi <washinpi@gmail.com>
2017-05-10 03:57:38 +0600

    Hello Wrold

---
+++ 1.hello-world.js
@@ -0,0 +1,12 @@
+
+//--https://www.codewars.com/kata/sentence-to-words/javascript
+
+/*-----------------------------------------/
+/-------- Return String to Array ----------/
+/-----------------------------------------*/
+
+function splitSentence(s) {
+  return s.split(' ');
+}
+
+//Example I/O : "hello world" -> ["hello", "world"]
